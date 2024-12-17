71a92eb55ca357ddf33c43db1d615160a9695dce
PetyaGGeorgieva <pettya.gg@gmail.com>
2024-12-17 17:02:35 +0100

    env file

--- util/location.js
+++ util/location.js
@@ -1,11 +1,11 @@
-const GOOGLE_API_KEY = 'AIzaSyDVVfTq1sixJBhKnVoSPea0mqVKWLX0j1w';
+import { EXPO_PUBLIC_GOOGLE_API_KEY } from '@env';
 
 export function getMapPreview(lat, lng) {
   if (lat === undefined || lng === undefined) {
     console.error('Invalid coordinates:', lat, lng);
     return '';
   }
-  const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x400&key=${GOOGLE_API_KEY}`;
-  console.log('Map preview URL:', imagePreviewUrl);
+  const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x400&key=${EXPO_PUBLIC_GOOGLE_API_KEY}`;
   return imagePreviewUrl;
 }
+
